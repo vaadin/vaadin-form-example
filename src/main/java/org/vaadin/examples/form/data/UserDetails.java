8238e2592b5ea75938e200bd0e8709130cd0112c
Manolo Carrasco <manolo@vaadin.com>
2023-03-13 09:01:26 +0100

    chore: update to 24.0.0

--- src/main/java/org/vaadin/examples/form/data/UserDetails.java
+++ src/main/java/org/vaadin/examples/form/data/UserDetails.java
@@ -1,7 +1,7 @@
 package org.vaadin.examples.form.data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
